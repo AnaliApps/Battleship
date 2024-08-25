3c2a47399908dbfaf8cf7a5cd89e8304d22ca513
AnaliApps <ronoanali5@gmail.com>
2024-08-25 16:41:59 +0300

    Player class implementation.
    -Built player to expect player names.
    -Added getter methods for getting these names.
    -Added two methods used to create boards for each player.

--- src/player/player.js
+++ src/player/player.js
@@ -1,13 +1,20 @@
+import GameBoard from "../board/gameBoard";
 class Player{
     constructor(_realPlayer,_computer){
         this.realPlayer = _realPlayer;
-        this.computer = _computer;
+        this.computerPlayer = _computer;
+    }
+    getRealPlayer(){
+        return this.realPlayer;
+    }
+    getComputerPlayer(){
+        return this.computerPlayer;
     }
     realPlayerBoard(){
-
+        return new GameBoard();
     }
     computerPlayerBoard(){
-
+        return new GameBoard();
     }
 }
 
